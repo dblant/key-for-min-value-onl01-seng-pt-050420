5a786d86aa72def9f0ea79ec2d18e2b9b644fbf9
Dustin Blanton <dustinblntn@gmail.com>
2020-05-16 00:42:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
  minimum_key, minimum_value = [nil, nil]
  
- name_hash.each do |value,key|
+ name_hash.each do |value,key| || == nil
    if value > minimum_value
      minimum_value = value
      minimum_keu - key
