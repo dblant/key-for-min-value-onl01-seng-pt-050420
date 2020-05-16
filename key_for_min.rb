7667f34dbd5fef6a3650e188600e90e84360c2f7
Dustin Blanton <dustinblntn@gmail.com>
2020-05-16 00:28:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key = 0 
+ value, 
   name_hash.each |key|
   if name_hash[key] > 0 
     key = min
